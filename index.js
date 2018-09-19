2b31089734ade21a5d486f3f7c7f0c086d4fee64
Alexander <atrubachov@gmail.com>
2018-09-19 12:23:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 "use strict"
 
 import PropTypes from 'prop-types';
+{ ViewPropTypes } from ‘react-native’
 import { requireNativeComponent, View } from 'react-native';
 
 module.exports = requireNativeComponent('RNTBgImage', {
 	name: 'RNTBgImage',
 	propTypes: {
 		drawable: PropTypes.string,
-		...View.propTypes // include the default view properties
+		...ViewPropTypes // include the default view properties
 	}
 });
